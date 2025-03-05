f451bd7fa7325bb47574450d0bae3487494b215d
Jordan Iralde <iraldejordan10@gmail.com>
2025-03-05 11:52:17 -0300

    App1

---
+++ Actividades2025/Actvidad1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ingrese el lado 1: ");
+int Lado1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el lado 2: ");
+int Lado2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el lado 3: ");
+int Lado3 = Convert.ToInt32(Console.ReadLine()); //Hipotenusa
+
+double Area = (base * altura) / 2;
+double Perimetro = Lado1 + Lado2 + Lado3;
+
+Console.WriteLine($"El área del triángulo equilátero es: {Area} y el perimetro es: {Perimetro}");
